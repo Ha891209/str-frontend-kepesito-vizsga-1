ad54041853f42340b4bc9b1795553fe13222fbc7
Aniko Kopis-Horvath <ha891209@gmail.com>
2021-05-31 22:29:56 +0200

    basic done

--- Project01/src/app/model/user.ts
+++ Project01/src/app/model/user.ts
@@ -1,2 +1,6 @@
 export class User {
+    id?: number;
+    image: string = '';
+    name: string = '';
+    commits: number = 0;
 }
--- Project01/src/app/service/user-service.service.ts
+++ Project01/src/app/service/user-service.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserServiceService {
+export class UserService {
 
-  constructor() { }
+  BaseURL: string = "https://api.github.com/repos/angular/angular/contributors?per_page=100"
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.BaseURL);
+  }
 }
